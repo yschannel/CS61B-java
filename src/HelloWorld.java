0044f72b065f77b3f37544c95f8bcb4efe0dde3e
yschannel <3391748651@qq.com>
2026-09-08 22:11:14 +0800

    feat: add HelloWorld code

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, CS61B 2024");
+
+        for (int i = 1; i <= 3; i++){
+            System.out.println("Lesson " + i + " completed!");
+        }
+    }
+}
