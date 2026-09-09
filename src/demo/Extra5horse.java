80978910aa1f6527e63dd4f343668796a021e1d6
yschannel <3391748651@qq.com>
2026-09-10 01:03:55 +0800

    add demo: Extra Thought Exercise (5horse)

---
+++ src/demo/Extra5horse.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Extra5horse {
+    public static void main(){
+        System.out.println(5 + "horse");
+        System.out.println(5 + " ");
+        System.out.println(5 + "10");
+        System.out.println(5 + 10);
+    }
+}
