e184b167a6f81b6dc8409539fcf4d02d5c8493cf
yschannel <3391748651@qq.com>
2026-09-18 00:25:54 +0800

    ch2 demo: Dog (static vs non-static)

---
+++ src/demo/ch2/dogs/Dog.java
@@ -0,0 +1,35 @@
+package demo.ch2.dogs;
+
+public class Dog {
+    public int weightInPounds;
+     public static String binomen = "Canis familiaris";
+
+    public Dog(int w) {
+        weightInPounds = w;
+    }
+
+    public void makeNoise() {
+        if (weightInPounds < 10) {
+            System.out.println("yip, yip, yip!");
+        } else if (weightInPounds < 30) {
+            System.out.println("bark, bark.");
+        } else {
+            System.out.println("woof!");
+        }
+    }
+
+    public static Dog maxDog(Dog d1, Dog d2){
+        if(d1.weightInPounds > d2.weightInPounds){
+            return d1;
+        }
+        return d2;
+    }
+
+    public Dog maxDog(Dog d2){
+        if(this.weightInPounds > d2.weightInPounds){
+            return this;
+        }
+        return d2;
+    }
+}
+
---
+++ src/demo/ch2/dogs/DogLauncher.java
@@ -0,0 +1,17 @@
+package demo.ch2.dogs;
+
+public class DogLauncher {
+    public static void main(String[] args){
+        Dog d = new Dog(20);
+        d.makeNoise();
+
+        Dog[] dogs = new Dog[2];
+        dogs[0] = new Dog(8);
+        dogs[1] = new Dog(40);
+
+        dogs[0].makeNoise();
+        dogs[1].makeNoise();
+
+
+    }
+}
